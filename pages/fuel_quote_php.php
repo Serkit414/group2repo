896aea95c6126de0a8f382ad996f765a76242443
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-02-16 19:05:39 -0600

    Update fuel_quote_php.php

--- pages/fuel_quote_php.php
+++ pages/fuel_quote_php.php
@@ -1,21 +1,55 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+<link rel="stylesheet" href="../styles.css">
+    <script src = "../js/formMover.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
 <body>
 
-<h1>Fuel Quote Form</h1>
+<div class = "quote-form">
+    <h2 class = "text-center">Fuel Quote Form</h2>
 
-<h2>Please confirm the following information:</h2>
-<br>
+    <form id = "fuelConf">
+        <h2 class = "text-center">Please confirm the following information:</h2>
+        <br>
 
-<span>Suggested Price/Gallon: $</span> <span id = "price">2.50</span> <br> <br>
-<span>Gallon Number: </span><span id = "number"><?php echo $_POST["GallonNumber"]; ?></span> <br>
-<p>Delivery Address: 123 Apple Dr, Houston, TX, 77204</p>
-<p>Delivery Date: <?php echo $_POST["DelDate"]; ?></p>
-<span>Total Cost: $</span> <span id = "costAmount"></span> <br>
+        <div class = "form-group">
+            <span>Suggested Price/Gallon: $</span> <span id = "price">2.50</span> <br>
+        </div>
 
-<button type = "button" onClick = "confirmInfo()">Confirm</button>
-<button type = "button" onClick = "denyInfo()">Deny</button>
+        <div class = "form-group">
+            <span>Gallon Number: </span><span id = "number"><?php echo $_POST["GallonNumber"]; ?></span> <br>
+        </div>
 
-<script src = "../js/confirmInfo.js"></script>
+        <div class = "form-group">
+            <p>Delivery Address: 123 Apple Dr, Houston, TX, 77204</p>
+        </div>
+
+        <div class = "form-group">
+            <p>Delivery Date: <?php echo $_POST["DelDate"]; ?></p>
+        </div>
+
+        <div class = "form-group">
+            <span>Total Cost: $</span> <span id = "costAmount"></span> <br>
+        </div>
+
+        <div class = "form-group">
+            <button type = "button" onClick = "confirmInfo()" class = "btn btn-primary btn-block">Confirm</button>
+        </div>
+
+        <div class = "form-group">
+            <button type = "button" onClick = "denyInfo()" class = "btn btn-primary btn-block">Deny</button>
+        </div>
+    </form>
+</div>
+
+<script src = "../js/priceCalc.js"></script>
 </body>
 </html> 
