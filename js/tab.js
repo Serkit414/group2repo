9e2f015db866e48b8f3da7432b44deb7571f73fb
Benjamin Bates <95107216+OneCookieLef@users.noreply.github.com>
2022-02-16 19:00:18 -0600

    Create tab.js

---
+++ js/tab.js
@@ -0,0 +1,24 @@
+function loginTab()
+{
+    window.location.href = "../pages/login.html";
+}
+
+function registerTab()
+{
+    window.location.href = "../pages/client_registration.html";
+}
+
+function profileTab()
+{
+    window.location.href = "../pages/client_profile_management.html";
+}
+
+function quoteTab()
+{
+    window.location.href = "../pages/fuel_quote_form.html";
+}
+
+function historyTab()
+{
+    window.location.href = "../pages/fuel_history.html";
+}
