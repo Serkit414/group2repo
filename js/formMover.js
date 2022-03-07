bb6ce0700d608dcb95946451972e9ae9e2e7c659
Benjamin Bates <95107216+OneCookieLef@users.noreply.github.com>
2022-03-06 22:49:25 -0600

    Add Profile management confirmation/deny buttons

--- js/formMover.js
+++ js/formMover.js
@@ -17,3 +17,13 @@ function historyLoader()
 {
     window.location.href = "../pages/fuel_history.html";
 }
+
+function confirmProf()
+{
+    window.location.href="../pages/client_profile_management.html"
+}
+
+function denyProf()
+{
+    window.location.href="../pages/client_profile_management.html"
+}
