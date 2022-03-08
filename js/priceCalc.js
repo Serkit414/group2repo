11ca9531286e42dd37d4967e66cdea69a70c5c43
Zachary Robinson <87831656+xnautsing04@users.noreply.github.com>
2022-03-08 09:54:01 -0600

    Delete priceCalc.js

--- js/priceCalc.js
+++
@@ -1,6 +0,0 @@
-let price = parseInt(document.getElementById("price").textContent);
-let gallonNumber = parseInt(document.getElementById("number").textContent);
-
-let totalCost = price * gallonNumber;
-
-document.getElementById("costAmount").textContent = totalCost;
